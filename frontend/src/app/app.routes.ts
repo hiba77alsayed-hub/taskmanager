d823c6898eb59caf0dcc578a725288306a435cc7
hiba77alsayed-hub <hiba77alsayed@gmail.com>
2026-03-16 12:05:13 +0300

    Added Routing, cleaned AppComponent and styled Login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect home to login
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'tasks',
+        loadComponent: () => import('./features/tasks/task-list/task-list').then(m => m.TaskList)
+    } // Lazy Loading for performance
+];
--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../../core/services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule], 
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class Login {
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(Auth);
+  private router = inject(Router);
 
+  // Define the form
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value as any).subscribe({
+        next: () => {
+          console.log('Logged in successfully!');
+          this.router.navigate(['/tasks']); // Move to tasks after login
+        },
+        error: (err) => alert('Login failed: ' + err.error.message)
+      });
+    }
+  }
 }
